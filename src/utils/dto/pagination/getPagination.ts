71c5cb7f55200d84db5e952b66c9f1184638e610
solomon-winner <solyalew3@gmail.com>
2025-06-20 12:15:44 +0300

    ...modifying my get pagination helper file...

--- src/utils/dto/pagination/getPagination.ts
+++ src/utils/dto/pagination/getPagination.ts
@@ -1,10 +1,19 @@
-// pagination.ts
-module.exports = function getPagination(ctx, defaultPageSize = 10) {
+module.exports = function getPaginationMeta(ctx, totalCount, defaultPageSize = 10) {
   const page = parseInt(ctx.query.page) || 1;
   const pageSize = parseInt(ctx.query.pageSize) || defaultPageSize;
 
-  const start = (page - 1) * pageSize;
-  const limit = pageSize;
+  const pageCount = Math.ceil(totalCount / pageSize);
 
-  return { start, limit };
+  return {
+    pagination: {
+      page,
+      pageSize,
+      pageCount,
+      total: totalCount,
+      hasNextPage: page < pageCount,
+      hasPrevPage: page > 1
+    },
+    start: (page - 1) * pageSize,
+    limit: pageSize
+  };
 };
