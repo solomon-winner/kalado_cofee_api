bac5ffa555f71cb7254ea9d7399b37101fcada58
solomon-winner <solyalew3@gmail.com>
2025-07-05 12:17:31 +0300

    ...

--- src/api/purchase-history/controllers/purchase-history.ts
+++ src/api/purchase-history/controllers/purchase-history.ts
@@ -3,13 +3,20 @@
  */
 
 import { factories } from '@strapi/strapi'
+import { verifyToken } from '../../../utils/dto/verify-token';
 
 export default factories.createCoreController('api::purchase-history.purchase-history', ({ strapi }) => ({
     async getPurchaseHistoryById(ctx) {
-        const { id } = ctx.params;
-    
-        const purchaseHistory = await strapi.entityService.findOne('api::purchase-history.purchase-history', id, {
-        populate: [ 'order'],
+        const decoded = verifyToken(ctx);
+        if (!decoded) {
+            return ctx.unauthorized('Invalid token');
+        }
+
+        const { id } = decoded.type === 'user' ? decoded.id : { id: ctx.params.id };
+
+        const purchaseHistory = await strapi.entityService.findMany('api::purchase-history.purchase-history', {
+            filters: { customer: id },
+            populate: ['order'],
         });
     
         if (!purchaseHistory) {
